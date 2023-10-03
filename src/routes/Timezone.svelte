c0c61c00e8ef8ae1574282ee66d26c300839d305
AipAkyol <a-akyol@hotmail.com>
2023-10-03 22:03:45 +0300

    few more fixes

--- src/routes/Timezone.svelte
+++ src/routes/Timezone.svelte
@@ -1,33 +1,28 @@
 <!-- src/routes/TimezoneDisplay.svelte -->
 <script>
-    import { onMount , onDestroy} from 'svelte';
-    let city="";
-    let currentTime;
-    let continent="";
-    let header = "Please enter the continent and the city";
-    let tempCity;
-    let tempContinent;
-    let isLoading = false;
-    let continentsData = {};
-    let citiesOfSelectedContinent = [];
-    let citySelectPlaceholder = "Please select a continent";
-    
-    function loadingState(){
-        isLoading = true;
-     }
+  import { onMount, onDestroy } from 'svelte';
 
-    function updateCityandContinent(){
-        city = tempCity;
-        continent = tempContinent;
-        tempCity = undefined;
-        tempContinent = undefined;
-    }
-    
-    function updateHeader(){
-        header = "Current Time in " + city;
-    }
+  let continent;
+  let city;
+  let currentTime;
+  let header = "Please enter the continent and the city";
+  let tempCity = "";
+  let tempContinent = "";
+  let isLoading = false;
+  let continentsData = {};
+  let citiesOfSelectedContinent = [];
+  let citySelectPlaceholder = "Please select a continent first";
+  let isClockStarted = false; // Add a flag to track whether the clock should be updated
 
-    async function fetchData() {
+  function loadingState() {
+    isLoading = true;
+  }
+
+  function updateURL() {
+    continent = tempContinent;
+    city = tempCity;
+  }
+  async function fetchData() {
     try {
       const response = await fetch('https://worldtimeapi.org/api/timezone');
       const data = await response.json();
@@ -36,8 +31,14 @@
       data.forEach(timezone => {
         const parts = timezone.split('/');
         const dataContinent = parts[0];
-        const dataCity = parts[1];
-        if ((dataCity == undefined) || (dataContinent == "Etc")){
+        let dataCity;
+        if (parts.length>2){
+          dataCity = parts[1] + "/" + parts[2];
+        }
+        else{
+          dataCity = parts[1];
+        }
+        if ((dataCity == undefined) || (dataContinent == "Etc")) {
           return;
         }
         // Create a continent entry if it doesn't exist
@@ -57,75 +58,78 @@
     fetchData();
   });
 
-  function changePlaceholder(){
-    citySelectPlaceholder = "Please select a city";
-  }
   function handleContinentChange(event) {
-    continent = event.target.value;
+    tempContinent = event.target.value;
     // Enable the city select and populate it with cities of the selected continent
-    citiesOfSelectedContinent = continentsData[continent] || [];
-    city = ''; // Reset selected city when the continent changes
-  }  
-    // Define a function to fetch the current time
-    async function fetchCurrentTime() {
-        try {
-          const response = await fetch(`https://worldtimeapi.org/api/timezone/${continent}/${city}`);
-            const data = await response.json();
-            currentTime = data.datetime.slice(11,19);
-            isLoading = false;
-        } catch (error) {
-            isLoading = false;
-            console.error('Error fetching data:', error);       
-        }
+    citiesOfSelectedContinent = continentsData[tempContinent] || [];
+    tempCity = ''; // Reset selected city when the continent changes
+
+    // Set the city select placeholder based on whether a continent is selected
+    citySelectPlaceholder = tempContinent ? "Please select a city" : "Please select a continent first";
+  }
+
+  async function fetchCurrentTime() {
+    if (!isClockStarted) return; // Do not fetch the time if the flag is not set
+    try {
+      const response = await fetch(`https://worldtimeapi.org/api/timezone/${continent}/${city}`);
+      const data = await response.json();
+      currentTime = data.datetime.slice(11, 19);
+      isLoading = false;
+    } catch (error) {
+      isLoading = false;
+      console.error('Error fetching data:', error);
     }
-  
-        // Initialize the interval when the component is mounted
+  }
+
   let interval;
   onMount(() => {
     interval = setInterval(fetchCurrentTime, 1000); // Update every 1000ms (1 second)
   });
 
-  // Clear the interval when the component is destroyed
   onDestroy(() => {
     clearInterval(interval);
   });
-  
-    onMount(fetchCurrentTime);
-  </script>
+
+</script>
 
 <main>
-    <h1>{header.replace(/_/g, " ")}</h1>
-    {#if isLoading}
-      <p>Loading...</p>
+  <h1>{header.replace(/_/g, " ")}</h1>
+  {#if isLoading}
+    <p>Loading...</p>
+  {:else}
+    {#if currentTime}
+      <p>{currentTime}</p>
     {:else}
-      {#if currentTime}
-        <p>{currentTime}</p>
-      {:else}
-        <p>Loading...</p>
-      {/if}
+      <p>Loading...</p>
     {/if}
-  </main>
+  {/if}
+</main>
 
-  <label for="continentSelect">Select a Continent:</label>
-  <select id="continentSelect" bind:value={tempContinent} on:change={handleContinentChange} on:change={changePlaceholder}>
-    {#each Object.keys(continentsData) as continent}
-      <option value={continent}>{continent}</option>
-    {/each}
-  </select>
-  
-  <label for="citySelect">Select a City:</label>
-  <select id="citySelect" bind:value={tempCity} disabled={!tempContinent}>
-    <option value="" disabled>{citySelectPlaceholder}</option>
-    {#each citiesOfSelectedContinent as city}
-      <option value={city}>{city.replace(/_/g, " ")}</option>
-    {/each}
-  </select>
-  
-  <button
-          on:click={loadingState}
-            on:click={updateCityandContinent} 
-             on:click={fetchCurrentTime} 
-              on:click={updateHeader} 
-              disabled={!tempCity}
-  >Submit</button>
-  
+<label for="continentSelect">Select a Continent:</label>
+<select id="continentSelect" bind:value={tempContinent} on:change={handleContinentChange}>
+  {#each Object.keys(continentsData) as continent}
+    <option value={continent}>{continent}</option>
+  {/each}
+</select>
+
+<label for="citySelect">Select a City:</label>
+<select id="citySelect" bind:value={tempCity} disabled={!tempContinent}>
+  <option value="" disabled>{citySelectPlaceholder}</option>
+  {#each citiesOfSelectedContinent as city}
+    <option value={city}>{city.replace(/_/g, " ")}</option>
+  {/each}
+</select>
+
+<button
+  on:click={loadingState}
+  on:click={updateURL}
+  on:click={() => {
+    isClockStarted = true; // Set the flag to start updating the clock
+    fetchCurrentTime(); // Manually fetch the time when the button is clicked
+  }}
+  on:click={()=> {
+    header = "Time in " + city.replace(/\//g, " ") + " is:"
+  }
+  } 
+  disabled={!tempCity}
+>Submit</button>
